0cd8ade47e99069e2a6448030b8a1a3880b6f51f
kavinraj06 <kavinraj@angleritech.com>
2018-07-16 09:59:13 +0530

    node sass was added

--- MurdochFestival/webpack.config.js
+++ MurdochFestival/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = (env) => {
             rules: [
                 { test: /\.tsx?$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
                 { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
+                { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader"] },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
