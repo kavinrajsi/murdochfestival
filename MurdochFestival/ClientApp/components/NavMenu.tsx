7a4301774a0a88c79d10dd3e916611ec857b99aa
kavinraj06 <kavinraj@angleritech.com>
2018-07-18 11:47:46 +0530

    change the menu height on scroll

--- MurdochFestival/ClientApp/components/NavMenu.tsx
+++ MurdochFestival/ClientApp/components/NavMenu.tsx
@@ -2,15 +2,30 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Link, NavLink } from 'react-router-dom';
 
-export class NavMenu extends React.Component<{}, { isToggleOn: true }> {
+export class NavMenu extends React.Component<{}, { isToggleOn: true, isTop: true }> {
     constructor(props:any) {
         super(props);
-        this.state = {isToggleOn: true};
+        this.state = {isToggleOn: true,
+            isTop: true
+        };
     
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
+        this.onScroll = this.onScroll.bind(this);
+
+      }
+      componentDidMount() {
+        document.addEventListener('scroll', () => {
+          const isTop = window.scrollY < 100;
+          if (isTop !== this.state.isTop) {
+            this.onScroll(isTop);
+          }
+        });
       }
     
+      onScroll(isTop:any) {
+        this.setState({ isTop });
+      }
       handleClick() {
         this.setState(prevState => ({
           isToggleOn: !prevState.isToggleOn
@@ -18,7 +33,7 @@ export class NavMenu extends React.Component<{}, { isToggleOn: true }> {
       }
     public render() {
         return <nav className='menu--bar'>
-                  <div className='menu--navigation'>
+                          <div className={this.state.isTop ? 'menu--navigation scrolling-down' : 'menu--navigation scrolling-up'}>
                   
           <div id="nav-burgermenu" onClick={this.handleClick} className=	{this.state.isToggleOn ? ' ': 'active'}>
           <span className=	{this.state.isToggleOn ? 'buger-menu-icon': 'buger-menu-icon active'}></span>
