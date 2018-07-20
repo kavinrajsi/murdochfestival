130ff5d9be651018fa6fac1740451c2bf5399800
kavinraj06 <kavinraj@angleritech.com>
2018-07-20 10:50:36 +0530

    menu closing issue in mobile is fixed

--- MurdochFestival/ClientApp/components/NavMenu.tsx
+++ MurdochFestival/ClientApp/components/NavMenu.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Link, NavLink } from 'react-router-dom';
 
-export class NavMenu extends React.Component<{}, { isToggleOn: true, isTop: true }> {
+export class NavMenu extends React.Component<{}, { isTop: true }> {
     constructor(props:any) {
         super(props);
-        this.state = {isToggleOn: true,
+        this.state = {
             isTop: true
         };
     
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
+
         this.onScroll = this.onScroll.bind(this);
 
       }
@@ -26,39 +25,35 @@ export class NavMenu extends React.Component<{}, { isToggleOn: true, isTop: true
       onScroll(isTop:any) {
         this.setState({ isTop });
       }
-      handleClick() {
-        this.setState(prevState => ({
-          isToggleOn: !prevState.isToggleOn
-        }));
-      }
+
     public render() {
         return <nav className='menu--bar'>
-                          <div className={this.state.isTop ? 'menu--navigation scrolling-down' : 'menu--navigation scrolling-up'}>
-                  
-          <div id="nav-burgermenu" onClick={this.handleClick} className=	{this.state.isToggleOn ? ' ': 'active'}>
-          <span className=	{this.state.isToggleOn ? 'buger-menu-icon': 'buger-menu-icon active'}></span>
-          </div>
-                    <ul data-role="menu"  className= {this.state.isToggleOn ? 'menu list-inline': 'menu list-inline menu-drawer-open'} >
-                        <li className="menu--item visible-xs">
-                            <a className="menu--item__link menu-buytickets"  href="#"  data-toggle="modal" data-target="#tickets" >Buy Tickets</a>
-                        </li>
-                        <li className="menu--item">
-                            <a className="menu--item__link" href="#artists">Artists</a>
-                        </li>
-                        <li className="menu--item">
-                            <a className="menu--item__link" href="#experiences">Experiences</a>
-                        </li>
-                        <li className="menu--item">
-                            <a className="menu--item__link" href="#festival-info">Festival Info</a>
-                        </li>
-                        <li className="menu--item">
-                            <a className="menu--item__link" href="#" data-toggle="modal" data-target="#stay-date">Subscribe</a>
-                        </li>
-                    </ul>
-                    <div className="masterbar--button">
-                        <a className="button--solid" href="#"  data-toggle="modal" data-target="#tickets" >Buy Tickets</a>
+                    <div className='menu--navigation'>
+                    
+                        <div id="nav-burgermenu">
+                            <span className='buger-menu-icon'></span>
+                        </div>
+                        <ul data-role="menu" className='menu list-inline' >
+                            <li className="menu--item visible-xs">
+                                <a className="menu--item__link menu-buytickets" href="#" data-toggle="modal" data-target="#tickets" >Buy Tickets</a>
+                            </li>
+                            <li className="menu--item">
+                                <a className="menu--item__link" href="#artists">Artists</a>
+                            </li>
+                            <li className="menu--item">
+                                <a className="menu--item__link" href="#experiences">Experiences</a>
+                            </li>
+                            <li className="menu--item">
+                                <a className="menu--item__link" href="#festival-info">Festival Info</a>
+                            </li>
+                            <li className="menu--item">
+                                <a className="menu--item__link" href="#" data-toggle="modal" data-target="#stay-date">Subscribe</a>
+                            </li>
+                        </ul>
+                        <div className="masterbar--button">
+                            <a className="button--solid" href="#" data-toggle="modal" data-target="#tickets" >Buy Tickets</a>
+                        </div>
                     </div>
-                   </div>
                 </nav>;
     }
 }
--- MurdochFestival/ClientApp/scripts/main.js
+++ MurdochFestival/ClientApp/scripts/main.js
@@ -13,4 +13,26 @@ $(document).ready(function() {
             scrollTop: $(target).offset().top - 110
         }, 400);
     });
+
+    var burgermenu  = ('#nav-burgermenu');
+    var burgermenuIcon  = ('.buger-menu-icon');
+    var menuList  = ('.menu.list-inline');
+
+    $('#nav-burgermenu').click(function(e){
+        e.preventDefault();
+        $(burgermenu).toggleClass('active');
+        $(burgermenuIcon).toggleClass('active');
+        $(menuList).toggleClass('menu-drawer-open');
+    });
+
+    $('.menu--item__link').click(function(e){
+        if($(window).width() <= 767){
+            e.preventDefault();
+            var target = $(this).attr('href');
+            $(burgermenu).toggleClass('active');
+            $(burgermenuIcon).toggleClass('active');
+            $(menuList).toggleClass('menu-drawer-open');
+        }
+    });
+
 });
